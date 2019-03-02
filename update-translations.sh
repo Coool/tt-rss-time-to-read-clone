baa84a696ae0cf3166f540e0475cc2522d2fb105
Andrew Dolgov <noreply@fakecake.org>
2019-03-02 08:44:28 +0300

    simplify rebase script

--- update-translations.sh
+++ update-translations.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 TEMPLATE=time_to_read.pot
 
-xgettext -kT_js_decl -kT_sprintf -k_ngettext:1,2 -kT_ngettext:1,2 -k__ -L PHP -o $TEMPLATE *.php
-
-xgettext --from-code utf-8 -k__ -kNotify.info -kNotify.error -kNotify.progress -kngettext -L Perl -j -o $TEMPLATE *.js
-
-xgettext --from-code utf-8 -k__ -kNotify.info -kNotify.error -kNotify.progress -kngettext -L Java -j -o $TEMPLATE *.js
-
+xgettext -kP__:2 -L PHP -o $TEMPLATE *.php
+xgettext -k__ -L Java -j -o $TEMPLATE *.js
 
 update_lang() {
 	if [ -f $1.po ]; then
@@ -24,5 +20,3 @@ for lang in $LANGS; do
 	PO_BASENAME=`echo $lang | sed s/.po//`
 	update_lang $PO_BASENAME
 done
-
-#./utils/update-js-translations.sh
