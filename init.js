20e94f987a8415673fbe760ef2d0ae17ab047d16
Andrew Dolgov <noreply@fakecake.org>
2019-03-10 09:20:45 +0300

    batch subscribe: stop dialog from being submitted twice

--- init.js
+++ init.js
@@ -9,7 +9,7 @@ require(['dojo/_base/kernel', 'dojo/ready'], function  (dojo, ready) {
 			if (ttr > 1) {
 
 				const pr = document.createElement("span");
-				pr.className = 'time-to-read insensitive';
+				pr.className = 'time-to-read text-muted';
 				pr.innerHTML = __("Time to read: ~%s minutes.").replace("%s", ttr);
 
 				row.querySelector(".titleWrap").appendChild(pr);
